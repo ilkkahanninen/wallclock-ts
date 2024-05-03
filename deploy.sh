b5c2ffda14906fd762f036f902f4c401f997db8f
Ilkka Hänninen <ilkka.hanninen@gmail.com>
2024-05-03 13:15:51 +0300

    Build frontend on deploy

--- deploy.sh
+++ deploy.sh
@@ -5,5 +5,7 @@ set -euo pipefail
 USER="deploy:$(op read "op://Private/stxx523yesdlbgnhdzdhddafk4/deployment password")"
 REPOSITORY="ilkkahanninen/wallclock-ts"
 
+make build-frontend
+git commit -am "Build: $(date)"
 git push
 curl -u "$USER" "https://ci.farjanverse.party/deploy/$REPOSITORY"
